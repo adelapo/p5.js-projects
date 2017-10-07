3d5452cba5c5d90c0e9c9f0fe4ebff40e640e906
adelapo <adelapo@berkeley.edu>
2017-10-06 22:06:00 -0700

    first commit

---
+++ Clock/sketch.js
@@ -0,0 +1,58 @@
+function setup() {
+	createCanvas(400, 400);
+	angleMode(DEGREES);
+}
+
+function draw() {
+	// Start by clearing the screen
+	clear();
+	
+	// (200, 200) --> (0, 0)
+	translate(200, 200);
+	
+	// Draw the circle.	
+	noStroke();
+	fill(0, 50, 98);
+	strokeWeight(8);
+	ellipse(0, 0, 400, 400);
+	
+	// Get the current time
+	var hr = hour() % 12;
+	var mn = minute();
+	var sc = second();
+	var mil = millis();
+	
+	// Set lengths of clock hands	
+	var hourHandLen = 110;
+	var minHandLen = 150;
+	var secHandLen = 180;
+	
+	// Calculate angle of clock hands at current time	
+	var hourAngle = map(hr, 0, 12, -90, 270);
+	var minAngle = map(mn, 0, 60, -90, 270);
+	var secAngle = map(sc, 0, 60, -90, 270);
+	
+	// Calculate X coordinate of clock hands at current time	
+	var hourHandX = hourHandLen * cos(hourAngle);	
+	var minHandX = minHandLen * cos(minAngle);
+	var secHandX = secHandLen * cos(secAngle);
+	
+	// Calculate Y coordinate of clock hands at current time
+	var hourHandY = hourHandLen * sin(hourAngle);
+	var minHandY = minHandLen * sin(minAngle);
+	var secHandY = secHandLen * sin(secAngle);
+	
+	stroke(255);
+	line(0, 0, hourHandX, hourHandY);
+	
+	stroke(255);
+	line(0, 0, minHandX, minHandY);
+	
+	stroke(235, 185, 23);
+	line(0, 0, secHandX, secHandY);
+	
+	stroke(0);	
+	point(0, 0);
+	
+	
+}
